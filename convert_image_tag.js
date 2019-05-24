e622374412bb2c1158aa9a9f9456f5f326519f6f
funzin <nakazawa.fumito@gmail.com>
2019-05-24 22:32:57 +0900

    Fix

--- convert_image_tag.js
+++ convert_image_tag.js
@@ -1,15 +1,15 @@
 var resultArray = [];
-const width="200px"
+var width="200px"
 
 var activeTextElement = document.activeElement;
 var textAreaStr = activeTextElement.value
 //var textAreaStr = '![haikei](https://user-images.githubusercontent.com/12893657/58318596-0d2dc700-7e53-11e9-860c-42c3cc42dc3f.jpeg) ![profile](https://user-images.githubusercontent.com/12893657/58318597-0dc65d80-7e53-11e9-8c7a-6c65e9e1b454.jpeg)'
 
-var mdImageArray = textAreaStr.match(/\!\[\S*\]\(https:\/\/user-images.githubusercontent.com\S+(jpg|jpeg|png)\)/gm);
+var mdImageArray = textAreaStr.match(/\!\[\S*\]\(https:\/\/user-images.githubusercontent.com\S+(jpg|jpeg|png)\)/gmi);
 if (mdImageArray != null) {
   for (let index in mdImageArray) {
     let mdImage = mdImageArray[index]
-    let imageURL = mdImage.match(/https:\S+(jpg|jpeg|png)/)[0];
+    let imageURL = mdImage.match(/https:\S+(jpg|jpeg|png)/i)[0];
     if (imageURL != undefined) {
       let imageTag = `<image src=${imageURL} width=${width}>`;
       resultArray.push([mdImage, imageTag])
