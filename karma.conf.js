90a4d0dfb4dceabbc3cde2bf15e54e14adf43059
funzin <nakazawa.fumito@gmail.com>
2019-08-15 16:04:40 +0900

    Add codecov

--- karma.conf.js
+++ karma.conf.js
@@ -32,14 +32,15 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      'src/html/*.html': ['html2js']
+      'src/html/*.html': ['html2js'],
+      'src/js/*.js': ['coverage']
     },
 
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'coverage'],
 
 
     // web server port
@@ -71,5 +72,12 @@ module.exports = function(config) {
     // Concurrency level
     // how many browser should be started simultaneous
     concurrency: Infinity,
+
+    coverageReporter: {
+      reporters: [
+        { type: 'lcovonly', subdir: '.' },
+        { type: 'json', subdir: '.' },
+        ]
+    }, 
   })
 }
